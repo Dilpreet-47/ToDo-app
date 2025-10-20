881bbe8f51827a4ade8128156929148b047e373b
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-10-20 14:49:19 +0530

    project setup

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
+  } catch (error) {
+    console.error("MONGODB not connected successfully", error);
+  }
+})();
