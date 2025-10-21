a150420336e936305c4ee2caa1c4f21baf39c480
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-10-21 18:06:52 +0530

    app and db connected

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-import { db_name } from "../constants";
+import { db_name } from "../constants.js";
 
-const connectDB = async () => {
+const connectDB = async () => { 
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${db_name}`
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
-import express from "express";
-import mongoose from "mongoose";
-import { db_name } from "./constants.js";
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`);
-    console.log("MONGODB CONNECTED SUCCESFULLY!!");
-
+connectDB()
+  .then(() => {
     app.on("error", (error) => {
-      console.log("Express app error!!", error);
+      console.log("Express app error", error);
       throw error;
     });
-
     app.listen(process.env.PORT || 5000, () => {
-      console.log(`App is listening at port ${process.env.PORT || 5000}`);
+      console.log(`Express app is listening on ${process.env.PORT}`);
     });
-  } catch (error) {
-    console.error("MONGODB CONNECTION FAILED!!", error);
-  }
-})();
+  })
+  .catch((error) => {
+    console.error("MONGO DB connection Failed!!", error);
+  });
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`);
+//     console.log("MONGODB CONNECTED SUCCESFULLY!!");
+
+//     app.on("error", (error) => {
+//       console.log("Express app error!!", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log(`App is listening at port ${process.env.PORT || 5000}`);
+//     });
+//   } catch (error) {
+//     console.error("MONGODB CONNECTION FAILED!!", error);
+//   }
+// })();
